65b674e423b48af9b607216f7ba07fed8ce6c0b3
letzzBuild <letzzBuild@gmail.com>
2021-12-06 19:33:43 +0530

    day17 notes added

---
+++ day17/code/my-app/src/App.js
@@ -0,0 +1,14 @@
+import Login from './Login'
+
+function App(){
+  return(
+    <>
+      <h1>Hello from React from App.js</h1>
+      <Login/>
+
+    </>
+  )
+}
+
+export default App
+
---
+++ day17/code/my-app/src/Login.jsx
@@ -0,0 +1,10 @@
+export default function Login(){
+ return(
+    <div>
+       <h1>Login Form</h1>
+       <input type="text" placeholder="Username"/>
+       <input type="password" placeholder="Password"/>
+       <button type="submit">Login</button>   
+    </div>
+ )   
+}
